c7817ecbfa4f46102c4693e42b5a6c3a5510f46e
Davi Fernandes <davi.fer159@gmail.com>
2024-12-04 17:20:30 -0300

    add section of disc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {
   Library,
   ChevronLeft,
   ChevronRight,
+  Play,
 } from "lucide-react";
 
 import Image from "next/image";
@@ -80,59 +81,164 @@ export default function Home() {
 
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
 
-          <div className="grid grid-cols-3 gap-4">
-            <div className="bg-white/10 rounded overflow-hidden">
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
+                width={86}
+                height={86}
                 src="/hybrid-theory.jpg"
                 alt="Capa do Album Hybrid Theory"
               />
-            </div>
+              <strong>Hybrid Theory</strong>
+              <button className="w-12 h-12 flex items-center justify-center  p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play fill="bg-black" />
+              </button>
+            </a>
 
-            <div className="bg-white/10 rounded overflow-hidden">
+            <a
+              href="#"
+              className="bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
+                width={86}
+                height={86}
                 src="/minutes-to-midnight.jpg"
                 alt="Capa do Album Minutes to Midnight"
               />
-            </div>
-            <div className="bg-white/10 rounded overflow-hidden">
+              <strong>Minutes to Midnight</strong>
+              <button className="w-12 h-12 flex items-center justify-center  p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play fill="bg-black" />
+              </button>
+            </a>
+
+            <a
+              href="#"
+              className="bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
+                width={86}
+                height={86}
                 src="/hybrid-theory.jpg"
                 alt="Capa do Album Hybrid Theory"
               />
-            </div>
+              <strong>Hybrid Theory</strong>
+              <button className="w-12 h-12 flex items-center justify-center  p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play fill="bg-black" />
+              </button>
+            </a>
 
-            <div className="bg-white/10 rounded overflow-hidden">
+            <a
+              href="#"
+              className="bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
+                width={86}
+                height={86}
                 src="/minutes-to-midnight.jpg"
                 alt="Capa do Album Minutes to Midnight"
               />
-            </div>
-            <div className="bg-white/10 rounded overflow-hidden">
+              <strong>Minutes to Midnight</strong>
+              <button className="w-12 h-12 flex items-center justify-center  p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play fill="bg-black" />
+              </button>
+            </a>
+
+            <a
+              href="#"
+              className="bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
+                width={86}
+                height={86}
                 src="/hybrid-theory.jpg"
                 alt="Capa do Album Hybrid Theory"
               />
-            </div>
+              <strong>Hybrid Theory</strong>
+              <button className="w-12 h-12 flex items-center justify-center  p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play fill="bg-black" />
+              </button>
+            </a>
 
-            <div className="bg-white/10 rounded overflow-hidden">
+            <a
+              href="#"
+              className="bg-white/5 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
               <Image
-                width={120}
-                height={120}
+                width={86}
+                height={86}
                 src="/minutes-to-midnight.jpg"
                 alt="Capa do Album Minutes to Midnight"
               />
-            </div>
+              <strong>Minutes to Midnight</strong>
+              <button className="w-12 h-12 flex items-center justify-center  p-2 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play fill="bg-black" />
+              </button>
+            </a>
           </div>
+
+          <h2 className="font-semibold text-2xl mt-10">Made for Davi Fernandes</h2>
+
+          <div className="grid grid-cols-10 gap-4 mt-4">
+            <a  href="#" className="bg-white/5 p-2 rounded-md hover:bg-white/10 flex flex-col gap-2"  >
+            <Image
+                className="w-full"
+                width={104}
+                height={104}
+                src="/hybrid-theory.jpg"
+                alt="Capa do Album Hybrid Theory"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">Chester, Mike, Dave, Rob, Brad</span>
+            </a>
+            <a href="#" className="bg-white/5 p-2 rounded-md hover:bg-white/10 flex flex-col gap-2"  >
+            <Image
+                className="w-full"
+                width={104}
+                height={104}
+                src="/hybrid-theory.jpg"
+                alt="Capa do Album Hybrid Theory"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">Chester, Mike, Dave, Rob, Brad</span>
+            </a>
+            <a href="#" className="bg-white/5 p-2 rounded-md hover:bg-white/10 flex flex-col gap-2"  >
+            <Image
+                className="w-full"
+                width={104}
+                height={104}
+                src="/hybrid-theory.jpg"
+                alt="Capa do Album Hybrid Theory"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">Chester, Mike, Dave, Rob, Brad</span>
+            </a>
+            <a href="#" className="bg-white/5 p-2 rounded-md hover:bg-white/10 flex flex-col gap-2"  >
+            <Image
+                className="w-full"
+                width={104}
+                height={104}
+                src="/hybrid-theory.jpg"
+                alt="Capa do Album Hybrid Theory"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">Chester, Mike, Dave, Rob, Brad</span>
+            </a>
+            <a href="#" className="bg-white/5 p-2 rounded-md hover:bg-white/10 flex flex-col gap-2"  >
+            <Image
+                className="w-full"
+                width={104}
+                height={104}
+                src="/hybrid-theory.jpg"
+                alt="Capa do Album Hybrid Theory"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-500">Chester, Mike, Dave, Rob, Brad</span>
+            </a>
+          </div>
+
         </main>
       </div>
 
